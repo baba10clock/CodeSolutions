d73f0f09b0be1f4bb22fc724fb5265960b131475
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-11 12:59:11 +0530

    Time: 0 ms (100%), Space: 42.5 MB (15.59%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cs
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cs
@@ -11,30 +11,19 @@
  */
 public class Solution {
     public ListNode RemoveNthFromEnd(ListNode head, int n) {
-        if(head.next == null) return null;
-        var len = 0;
-        var temp = head;
-        while(temp != null)
-        {
-            len++;
-            temp = temp.next;
-        }
-
-        var res = new ListNode();
-        res.next = head;
-        var times = len - n - 1;
-        while(times > 0 && head != null)
-        {
-            times--;
-            head = head.next;
-        }
-
-        if(len == n)
-            return head.next;
-
-        if(head.next != null)
-            head.next = head.next.next;
+        var res = new ListNode(0, head);
+        var fast = res;
+        var slow = res;
+        for(int i = 0; i <= n; i++)
+            fast = fast.next;
         
+        while(fast != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
         return res.next;
     }
 }
