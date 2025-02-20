11ab808d515ab867b3c42fefcbad2c650d045f6f
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-20 22:15:46 +0530

    Time: 195 ms (77.68%), Space: 65 MB (55.95%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int good;
+    public int GoodNodes(TreeNode root) {
+        good = 0;
+        Verify(root, root.val);
+        return good;
+    }
+    public void Verify(TreeNode root, long max)
+    {
+        if(root == null)    return;
+        if(root.val >= max)
+        {
+                good++;
+                max = root.val;
+        }
+        Verify(root.left, max);
+        Verify(root.right, max); 
+        return;
+    }
+}
