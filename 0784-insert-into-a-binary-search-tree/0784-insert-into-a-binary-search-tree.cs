3c029e163d15b19ea1d76fb5cb92f3aeeff27be3
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-24 21:19:57 +0530

    Time: 0 ms (100%), Space: 55.3 MB (41.22%) - LeetHub

---
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InsertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        Insert(root, val);
+        return root;
+    }
+    public void Insert(TreeNode root, int val)
+    {
+        if(root.val > val)
+        {
+            if(root.left != null)
+            {
+                Insert(root.left, val);
+            }
+            else
+            {
+                root.left = new TreeNode(val);
+                return;
+            }
+        }
+        if(root.val < val)
+        {
+            if(root.right != null)
+            {
+                Insert(root.right, val);
+            }
+            else
+            {
+                root.right = new TreeNode(val);
+                return;
+            }
+        }
+        return;
+    }
+}
