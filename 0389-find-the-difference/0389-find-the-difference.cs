3386448ff9fde2e312e6877a53447f91c1ab1c48
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-25 14:44:33 +0530

    Time: 1 ms (75.45%), Space: 42.5 MB (46.48%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,22 +1,8 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
-        var smap = new Dictionary<char, int>();
-        foreach(var i in s)
-        {
-            smap[i] = smap.GetValueOrDefault(i) + 1;
-        }
-        foreach(var i in t)
-        {
-            if(smap.ContainsKey(i))
-            {
-                if(smap[i] == 0)    return i;
-                smap[i]--;
-            }
-            else
-            {
-                return i;
-            }
-        }
-        return 'i';
+        var res = '\0';
+        foreach(var i in s) res ^= i;
+        foreach(var i in t) res ^= i;
+        return res;
     }
 }
