a6f6388b32b766bbab6ae707c2d931eff79f2336
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-02-27 15:04:08 +0530

    Time: 40 ms (89.79%), Space: 69.4 MB (6.05%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cs
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cs
@@ -1,20 +1,5 @@
 public class Solution {
     public int[][] KClosest(int[][] points, int k) {
-        var pq = new PriorityQueue<int[],int>();
-        foreach(var point in points)
-        {
-            var dist = point[0]*point[0] + point[1]*point[1];
-            pq.Enqueue(point, -dist);
-            if(pq.Count > k)
-            {
-                pq.Dequeue();
-            }
-        }
-        var res = new int [k][];
-        for(int i = 0; i < k; i++)
-        {
-            res[i] = pq.Dequeue();
-        }
-        return res;
+       return points.OrderBy(i => i[0]*i[0] + i[1]*i[1]).Take(k).ToArray();
     }
 }
