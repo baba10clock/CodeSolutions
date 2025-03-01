87850d25baa50c21978dfa846bbe8777027d2d41
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-01 19:04:55 +0530

    Time: 6 ms (51.12%), Space: 67.8 MB (47.03%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+
+        var lst = new List<string>();
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)    lst.Add("FizzBuzz");
+            else if(i % 3 == 0) lst.Add("Fizz");
+            else if(i % 5 == 0) lst.Add("Buzz");
+            else    lst.Add(i.ToString());
+        }
+        return lst;
+    }
+}
