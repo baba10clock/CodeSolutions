3d1ced3e8737851dd08b5ff9d65a408cdc6446ab
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-05 22:32:25 +0530

    Time: 4 ms (100%), Space: 43.8 MB (71.43%) - LeetHub

---
+++ 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int MinNumberOfFrogs(string croakOfFrogs) {
+        int c = 0, r = 0, o = 0, a = 0, k = 0, frog = 0, maxFrogs = 0;
+        foreach(var voice in croakOfFrogs)
+        {
+            switch(voice)
+            {
+                case 'c':
+                    c++;
+                    frog++;
+                    maxFrogs = Math.Max(maxFrogs, frog);
+                    break;
+                case 'r':
+                    if(c == 0)  return -1;
+                    c--;
+                    r++;
+                    break;
+                case 'o':
+                    if(r == 0) return -1;
+                    r--;
+                    o++;
+                    break;
+                case 'a':
+                    if(o == 0)  return -1;
+                    o--;
+                    a++;
+                    break;
+                case 'k':
+                    if(a == 0) return -1;
+                    a--;
+                    k++;
+                    frog--;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+        if(c == 0 && r == 0 && o == 0 && a == 0)
+            return maxFrogs;
+        return -1;
+    }
+}
