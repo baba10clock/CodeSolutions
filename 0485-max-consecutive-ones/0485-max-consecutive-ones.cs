c72683424623c68e5c59e28fc043129e0aae203f
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-05 22:43:21 +0530

    Time: 1 ms (100%), Space: 59.6 MB (44.05%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int curr = 0, maxx = int.MinValue;
+        foreach(var num in nums)
+        {
+            if(num == 1)
+                curr++;
+            else
+            {
+                maxx = Math.Max(curr, maxx);
+                curr = 0;
+            }
+        }
+        maxx = Math.Max(curr, maxx);
+        return maxx;
+    }
+}
