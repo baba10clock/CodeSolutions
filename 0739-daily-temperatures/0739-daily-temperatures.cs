6da28e788d9da9f7d3d610fb46be8270b10c9ed7
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-08 00:00:00 +0530

    Time: 13 ms (44.42%), Space: 67.4 MB (71.58%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        var stk = new Stack<(int,int)>();
+        var res = new int[temperatures.Length];
+        for(int i = 0; i < temperatures.Length; i++)
+        {
+            int t = temperatures[i];
+            while(stk.Count > 0 && t > stk.Peek().Item1)
+            {
+                var tuple = stk.Pop();
+                res[tuple.Item2] = i - tuple.Item2;
+            }
+            stk.Push((t,i));
+        }
+        return res;
+    }
+}
