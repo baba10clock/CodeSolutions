2bc2e02d75778a0870edf05979f7b5d4d3183e81
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-07 22:15:07 +0530

    Time: 11 ms (90.01%), Space: 69.3 MB (71.73%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        var map = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(map.ContainsKey(nums[i]))
+            {
+                if(Math.Abs(i - map[nums[i]]) <= k)   return true;
+                else
+                    map[nums[i]] = i;
+            }
+            else
+            {
+                map[nums[i]] = i; // val : index
+            }
+        }
+        return false;
+    }
+}
