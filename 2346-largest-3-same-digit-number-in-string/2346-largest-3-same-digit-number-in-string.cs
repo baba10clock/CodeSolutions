1a6511c549253c31685fd295227d85d3a36f090e
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-13 18:48:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (68.18%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cs
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cs
@@ -1,23 +1,12 @@
 public class Solution {
     public string LargestGoodInteger(string num) {
-        int i = 0, j = 1, k = 2;
-        var lst = new List<string>();
-        var res = "0";
-        while(k < num.Length)
+        string [] nums = new string [] {"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+        foreach(var i in nums)
         {
-            if(num[i] == num[j] && num[j] == num[k])
-            {
-                var temp = string.Concat(num[i],num[j], num[k]);
-                if(int.TryParse(temp, out int val1) && int.TryParse(res, out int val2))
-                {
-                    if(val1 >= val2)
-                        res = temp;
-                }
-            }
-            i++;
-            j++;
-            k++;
-        }
-        return res == "0" ? "" : res;
+            if(num.Contains(i))
+                return i;
+        } 
+        return "";
     }
+
 }
