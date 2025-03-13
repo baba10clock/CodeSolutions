bd375436cdf67e8b50bd1622514aa60c723e97e9
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-13 19:28:00 +0530

    Time: 130 ms (68.85%), Space: 48.4 MB (27.87%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cs
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cs
@@ -1,10 +1,28 @@
 public class Solution {
     public int MaxProductDifference(int[] nums) {
-        Array.Sort(nums);
-        int a = nums[^1];
-        int b = nums[^2];
-        int c = nums[0];
-        int d = nums[1];
-        return a*b - c*d;
+        int max1 = int.MinValue, max2 = int.MinValue, min1 = int.MaxValue, min2 = int.MaxValue;
+        foreach(var ele in nums)
+        {
+            if(ele > max1)
+            {
+                max2 = max1;
+                max1 = ele;
+            }
+            else if(ele > max2)
+            {
+                max2 = ele;
+            }
+
+            if(min1 > ele)
+            {
+                min2 = min1;
+                min1 = ele;
+            }
+            else if(min2 > ele)
+            {
+                min2 = ele;
+            }
+        }
+        return max1 * max2 - min1 * min2;
     }
 }
