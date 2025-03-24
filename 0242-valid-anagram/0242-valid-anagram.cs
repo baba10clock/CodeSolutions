6feadf6d94e879d827027f2c06eaaf021b1a40c1
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-24 22:25:05 +0530

    Time: 0 ms (100%), Space: 43.6 MB (59.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,28 +1,21 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        if(s.Length != t.Length)    return false;
-        var sMap = new Dictionary<char, int>();
-        var tMap = new Dictionary<char, int>();
+        if(s.Length != t.Length)
+        {
+            return false;
+        }
+        var res = new int[26];
         foreach(var i in s)
         {
-            sMap[i] = (sMap.ContainsKey(i) ? sMap[i] : 0) + 1;
-        }
-
+            res[i - 'a']++;
+        } 
         foreach(var i in t)
         {
-            tMap[i] = (tMap.ContainsKey(i) ? tMap[i] : 0) + 1;
-        }
-
-        foreach(var key in sMap.Keys)
-        {
-            if(tMap.ContainsKey(key))
+            if(--res[i - 'a'] < 0)
             {
-                if(sMap[key] != tMap[key]) return false;
-            }
-            else
                 return false;
+            }
         }
-
         return true;
     }
 }
