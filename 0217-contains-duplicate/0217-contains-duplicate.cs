9b1befae3cd3791bdaa5db046d3979075939df15
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-03-24 22:26:11 +0530

    Time: 12 ms (72.36%), Space: 57.3 MB (84.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,14 +1,5 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        var set = new HashSet<int>();
-        foreach(var i in nums)
-        {
-            if(set.Contains(i))
-            {
-                return true;
-            }
-            set.Add(i);
-        }
-        return false;
+        return nums.Length != new HashSet<int>(nums).Count;
     }
 }
