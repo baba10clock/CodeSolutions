2a8a24d37c6a3d13574df477a290ca4912e6a363
Vikas Chauhan <77842207+baba10clock@users.noreply.github.com>
2025-04-04 21:43:16 +0530

    Time: 1 ms (100%), Space: 60.8 MB (84.91%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.cs
@@ -0,0 +1,43 @@
+public class ParkingSystem {
+    private int bigCount, mediumCount, smallCount;
+
+    public ParkingSystem(int big, int medium, int small) {
+        bigCount = big;
+        mediumCount = medium;
+        smallCount = small; 
+    }
+    
+    public bool AddCar(int carType) {
+        switch(carType)
+        {
+            case 1:
+                if(bigCount > 0)
+                {
+                    bigCount--;
+                    return true;
+                }
+                return false;
+            case 2:
+                if(mediumCount > 0)
+                {
+                    mediumCount--;
+                    return true;
+                }
+                return false;
+            case 3:
+                if(smallCount > 0)
+                {
+                    smallCount--;
+                    return true;
+                }
+                return false;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
